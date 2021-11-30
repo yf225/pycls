ea3e6d165aba462b33885e9279fbc73211b3887a
Will Feng (DPER) <willfeng@fb.com>
2021-11-30 11:07:29 -0800

    update

---
+++ train_vit_pt_pycls_gpu.py
@@ -0,0 +1,487 @@
+#!/usr/bin/env python3
+
+"""On AWS GPU node
+conda activate torch-1.10
+
+cd /fsx/users/willfeng/repos
+rm -rf ./pycls || true
+git clone https://github.com/yf225/pycls.git -b vit_dummy_data
+cd pycls && git pull
+export PYTHONPATH=/fsx/users/willfeng/repos/pytorch-image-models:${PYTHONPATH}
+
+python -m torch.distributed.launch --nproc_per_node=4 \
+train_vit_pt_pycls_gpu.py --apex-amp --apex-amp-opt-level=O3 --mode=eager --micro_batch_size=2
+
+python -m torch.distributed.launch --nproc_per_node=4 \
+train_vit_pt_pycls_gpu.py --mode=graph --micro_batch_size=2
+
+python -m torch.distributed.launch --nproc_per_node=4 \
+train_vit_pt_pycls_gpu.py --mode=eager --micro_batch_size=4
+"""
+import argparse
+import time
+import os
+import logging
+import statistics
+from collections import OrderedDict
+from contextlib import suppress
+from datetime import datetime
+
+import torch
+import torch.nn as nn
+import torchvision.utils
+from torch.nn.parallel import DistributedDataParallel as NativeDDP
+import einops
+
+from timm.data import create_dataset, create_loader, resolve_data_config, Mixup, FastCollateMixup, AugMixDataset
+from timm.models import create_model, safe_model_name, resume_checkpoint, load_checkpoint,\
+    convert_splitbn_model, model_parameters
+from timm.utils import *
+from timm.loss import *
+from timm.optim import create_optimizer_v2, optimizer_kwargs
+from timm.scheduler import create_scheduler
+from timm.utils import ApexScaler, NativeScaler
+from timm.models.helpers import build_model_with_cfg
+from timm.models.vision_transformer import VisionTransformer
+
+try:
+    from apex import amp
+    from apex.parallel import DistributedDataParallel as ApexDDP
+    from apex.parallel import convert_syncbn_model
+    has_apex = True
+except ImportError:
+    has_apex = False
+
+has_native_amp = False
+try:
+    if getattr(torch.cuda.amp, 'autocast') is not None:
+        has_native_amp = True
+except AttributeError:
+    pass
+
+try:
+    import wandb
+    has_wandb = True
+except ImportError:
+    has_wandb = False
+
+torch.backends.cudnn.benchmark = True
+
+
+# Hyperparams
+
+VERBOSE = False
+num_attention_heads = 16
+hidden_size = 1280
+num_layers = 32
+
+image_size = 224
+patch_size = 16  # Size of the patches to be extract from the input images
+
+num_classes = 1000
+num_epochs = 10
+
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+
+# Batch size
+parser.add_argument("--micro_batch_size", default=32, type=int)
+
+# Optimizer parameters
+parser.add_argument('--clip-grad', type=float, default=None, metavar='NORM',
+                    help='Clip gradient norm (default: None, no clipping)')
+parser.add_argument('--clip-mode', type=str, default='norm',
+                    help='Gradient clipping mode. One of ("norm", "value", "agc")')
+
+# Misc
+parser.add_argument('--amp', action='store_true', default=False,
+                    help='use NVIDIA Apex AMP or Native AMP for mixed precision training')
+parser.add_argument('--apex-amp', action='store_true', default=False,
+                    help='Use NVIDIA Apex AMP mixed precision')
+parser.add_argument('--apex-amp-opt-level', type=str, default='O1',
+                    help='NVIDIA Apex AMP optimization level ("O0", "O1", "O2", "O3")')
+parser.add_argument('--native-amp', action='store_true', default=False,
+                    help='Use Native Torch AMP mixed precision')
+parser.add_argument('--channels-last', action='store_true', default=False,
+                    help='Use channels_last memory layout')
+parser.add_argument('--pin-mem', action='store_true', default=False,
+                    help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+parser.add_argument("--local_rank", default=0, type=int)
+parser.add_argument('--use-multi-epochs-loader', action='store_true', default=False,
+                    help='use the multi-epochs-loader to save time at the beginning of every epoch')
+parser.add_argument('--mode', type=str,
+                    help='"eager" or "graph"')
+parser.add_argument('--log-interval', type=int, default=1, metavar='N',
+                    help='how many batches to wait before logging training status')
+
+def print_if_verbose(msg):
+    if VERBOSE:
+        print(msg, flush=True)
+
+class VitDummyDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_size, crop_size, num_classes):
+        self.dataset_size = dataset_size
+        self.crop_size = crop_size
+        self.num_classes = num_classes
+
+    def __len__(self):
+        return self.dataset_size
+
+    def __getitem__(self, index):
+        return (torch.rand(3, self.crop_size, self.crop_size).to(torch.half), torch.randint(self.num_classes, (1,)).to(torch.long))
+
+
+import math
+
+import numpy as np
+import torch
+from pycls.core.config import cfg
+from pycls.models.blocks import (
+    MultiheadAttention,
+    activation,
+    conv2d,
+    conv2d_cx,
+    layernorm,
+    layernorm_cx,
+    linear,
+    linear_cx,
+    norm2d,
+    norm2d_cx,
+    patchify2d,
+    patchify2d_cx,
+)
+from torch.nn import Module, Parameter, init
+
+
+class ViTHead(Module):
+    """Transformer classifier, an fc layer."""
+
+    def __init__(self, w_in, num_classes):
+        super().__init__()
+        self.head_fc = linear(w_in, num_classes, bias=True)
+
+    def forward(self, x):
+        return self.head_fc(x)
+
+
+class MLPBlock(Module):
+    """Transformer MLP block, fc, gelu, fc."""
+
+    def __init__(self, w_in, mlp_d):
+        super().__init__()
+        self.linear_1 = linear(w_in, mlp_d, bias=True)
+        self.af = activation("gelu")
+        self.linear_2 = linear(mlp_d, w_in, bias=True)
+
+    def forward(self, x):
+        return self.linear_2(self.af(self.linear_1(x)))
+
+
+class ViTEncoderBlock(Module):
+    """Transformer encoder block, following https://arxiv.org/abs/2010.11929."""
+
+    def __init__(self, hidden_d, n_heads, mlp_d):
+        super().__init__()
+        self.ln_1 = layernorm(hidden_d)
+        self.self_attention = MultiheadAttention(hidden_d, n_heads)
+        self.ln_2 = layernorm(hidden_d)
+        self.mlp_block = MLPBlock(hidden_d, mlp_d)
+
+    def forward(self, x):
+        x_p = self.ln_1(x)
+        x_p, _ = self.self_attention(x_p, x_p, x_p)
+        x = x + x_p
+        x_p = self.mlp_block(self.ln_2(x))
+        return x + x_p
+
+
+class ViTEncoder(Module):
+    """Transformer encoder (sequence of ViTEncoderBlocks)."""
+
+    def __init__(self, n_layers, hidden_d, n_heads, mlp_d):
+        super(ViTEncoder, self).__init__()
+        for i in range(n_layers):
+            self.add_module(f"block_{i}", ViTEncoderBlock(hidden_d, n_heads, mlp_d))
+        self.ln = layernorm(hidden_d)
+
+    def forward(self, x):
+        for block in self.children():
+            x = block(x)
+        return x
+
+
+class ViTStemPatchify(Module):
+    """The patchify vision transformer stem as per https://arxiv.org/abs/2010.11929."""
+
+    def __init__(self, w_in, w_out, k):
+        super(ViTStemPatchify, self).__init__()
+        self.patchify = patchify2d(w_in, w_out, k, bias=True)
+
+    def forward(self, x):
+        return self.patchify(x)
+
+
+class ViT(Module):
+    """Vision transformer as per https://arxiv.org/abs/2010.11929."""
+
+    @staticmethod
+    def check_params(params):
+        p = params
+        err_str = "Input shape indivisible by patch size"
+        assert p["image_size"] % p["patch_size"] == 0, err_str
+        assert p["stem_type"] in ["patchify", "conv"], "Unexpected stem type"
+        assert p["cls_type"] in ["token", "pooled"], "Unexpected classifier mode"
+        if p["stem_type"] == "conv":
+            err_str = "Conv stem layers mismatch"
+            assert len(p["c_stem_dims"]) == len(p["c_stem_strides"]), err_str
+            assert len(p["c_stem_strides"]) == len(p["c_stem_kernels"]), err_str
+            err_str = "Stem strides unequal to patch size"
+            assert p["patch_size"] == np.prod(p["c_stem_strides"]), err_str
+            err_str = "Stem output dim unequal to hidden dim"
+            assert p["c_stem_dims"][-1] == p["hidden_d"], err_str
+
+    def __init__(self, params=None):
+        super(ViT, self).__init__()
+        p = ViT.get_params() if not params else params
+        ViT.check_params(p)
+        self.stem = ViTStemPatchify(3, p["hidden_d"], p["patch_size"])
+        seq_len = (p["image_size"] // cfg.VIT.PATCH_SIZE) ** 2
+        self.class_token = None
+        self.pos_embedding = Parameter(torch.zeros(seq_len, 1, p["hidden_d"]))
+        self.encoder = ViTEncoder(
+            p["n_layers"], p["hidden_d"], p["n_heads"], p["mlp_d"]
+        )
+        self.head = ViTHead(p["hidden_d"], p["num_classes"])
+
+    def forward(self, x):
+        # (n, c, h, w) -> (n, hidden_d, n_h, n_w)
+        x = self.stem(x)
+        # (n, hidden_d, n_h, n_w) -> (n, hidden_d, (n_h * n_w))
+        x = x.reshape(x.size(0), x.size(1), -1)
+        # (n, hidden_d, (n_h * n_w)) -> ((n_h * n_w), n, hidden_d)
+        x = x.permute(2, 0, 1)
+        x = x + self.pos_embedding
+        x = self.encoder(x)
+        return self.head(x)
+
+
+step_duration_list = []
+
+def main():
+    args = parser.parse_args()
+
+    args.distributed = False
+    if 'WORLD_SIZE' in os.environ:
+        args.distributed = int(os.environ['WORLD_SIZE']) > 1
+    args.device = 'cuda:0'
+    args.world_size = 1
+    args.rank = 0  # global rank
+    if args.distributed:
+        args.device = 'cuda:%d' % args.local_rank
+        torch.cuda.set_device(args.local_rank)
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+        args.world_size = torch.distributed.get_world_size()
+        args.rank = torch.distributed.get_rank()
+        print_if_verbose('Training in distributed mode with multiple processes, 1 GPU per process. Process %d, total %d.'
+                     % (args.rank, args.world_size))
+    else:
+        print_if_verbose('Training with a single process on 1 GPUs.')
+    assert args.rank >= 0
+
+    # resolve AMP arguments based on PyTorch / Apex availability
+    use_amp = None
+    if args.amp:
+        # `--amp` chooses native amp before apex (APEX ver not actively maintained)
+        if has_native_amp:
+            args.native_amp = True
+        elif has_apex:
+            args.apex_amp = True
+    if args.apex_amp and has_apex:
+        use_amp = 'apex'
+    elif args.native_amp and has_native_amp:
+        use_amp = 'native'
+    elif args.apex_amp or args.native_amp:
+        print_if_verbose("Neither APEX or native Torch AMP is available, using float32. "
+                        "Install NVIDA apex or upgrade to PyTorch 1.6")
+
+    random_seed(42, args.rank)
+
+    model = ViT(
+        {
+            "image_size": image_size,
+            "patch_size": patch_size,
+            "n_layers": num_layers,
+            "n_heads": num_attention_heads,
+            "hidden_d": hidden_size,
+            "mlp_d": 4 * hidden_size,
+            "num_classes": num_classes,
+        }
+    )
+
+    if args.local_rank == 0:
+        print_if_verbose(
+            f'Model created, param count:{sum([m.numel() for m in model.parameters()])}')
+
+    # move model to GPU, enable channels last layout if set
+    if use_amp != 'apex':
+        model = model.to(torch.half)
+    else:
+        # When using apex.amp.initialize, you do not need to call .half() on your model
+        # before passing it, no matter what optimization level you choose.
+        pass
+    if args.channels_last:
+        model = model.to(memory_format=torch.channels_last)
+
+    assert args.mode in ["eager", "graph"]
+    if args.mode == "graph":
+        assert not use_amp == 'apex', 'Cannot use APEX AMP with torchscripted model'
+        model = torch.jit.script(torch.fx.symbolic_trace(model))
+
+    model = model.cuda()
+
+    optimizer = create_optimizer_v2(model, 'adam')
+
+    # setup automatic mixed-precision (AMP) loss scaling and op casting
+    amp_autocast = suppress  # do nothing
+    loss_scaler = None
+    if use_amp == 'apex':
+        assert args.apex_amp_opt_level == 'O3', "only half-pr\ecision is supported!"
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.apex_amp_opt_level)
+        loss_scaler = ApexScaler()
+        if args.local_rank == 0:
+            print_if_verbose('Using NVIDIA APEX AMP. Training in mixed precision.')
+    elif use_amp == 'native':
+        amp_autocast = torch.cuda.amp.autocast
+        loss_scaler = NativeScaler()
+        if args.local_rank == 0:
+            print_if_verbose('Using native Torch AMP. Training in mixed precision.')
+    else:
+        if args.local_rank == 0:
+            print_if_verbose('AMP not enabled.')
+
+    # setup distributed training
+    if args.distributed:
+        if has_apex and use_amp == 'apex':
+            # Apex DDP preferred unless native amp is activated
+            if args.local_rank == 0:
+                print_if_verbose("Using NVIDIA APEX DistributedDataParallel.")
+            model = ApexDDP(model, delay_allreduce=True)
+        else:
+            if args.local_rank == 0:
+                print_if_verbose("Using native Torch DistributedDataParallel.")
+            model = NativeDDP(model, device_ids=[args.local_rank])
+        # NOTE: EMA model does not need to be wrapped by DDP
+
+    start_epoch = 0
+
+    if args.local_rank == 0:
+        print_if_verbose('Scheduled epochs: {}'.format(num_epochs))
+
+    # create train dataset
+    dataset_train = VitDummyDataset(args.micro_batch_size * torch.distributed.get_world_size() * 10, image_size, num_classes)
+
+    loader_train = create_loader(
+        dataset_train,
+        input_size=(3, 224, 224),
+        batch_size=args.micro_batch_size * torch.distributed.get_world_size(),
+        is_training=True,
+        no_aug=True,
+        fp16=True,
+    )
+
+    # setup loss function
+    train_loss_fn = nn.CrossEntropyLoss().to(torch.half)
+    train_loss_fn = train_loss_fn.cuda()
+
+    try:
+        for epoch in range(start_epoch, num_epochs):
+            train_metrics = train_one_epoch(
+                epoch, model, loader_train, optimizer, train_loss_fn, args,
+                amp_autocast=amp_autocast, loss_scaler=loss_scaler)
+        if args.local_rank == 0:
+            print("micro_batch_size: {}, mean step duration: {:.3f}".format(args.micro_batch_size, statistics.median(step_duration_list)))
+    except KeyboardInterrupt:
+        pass
+
+
+def train_one_epoch(
+        epoch, model, loader, optimizer, loss_fn, args,
+        amp_autocast=suppress, loss_scaler=None):
+
+    second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
+    batch_time_m = AverageMeter()
+    data_time_m = AverageMeter()
+    losses_m = AverageMeter()
+
+    model.train()
+
+    end = time.time()
+    last_idx = len(loader) - 1
+    num_updates = epoch * len(loader)
+    for batch_idx, (input, target) in enumerate(loader):
+        last_batch = batch_idx == last_idx
+        data_time_m.update(time.time() - end)
+        if args.channels_last:
+            input = input.contiguous(memory_format=torch.channels_last)
+
+        with amp_autocast():
+            output = model(input)
+            loss = loss_fn(output, target)
+
+        if not args.distributed:
+            losses_m.update(loss.item(), input.size(0))
+
+        optimizer.zero_grad()
+        if loss_scaler is not None:
+            loss_scaler(
+                loss, optimizer,
+                clip_grad=args.clip_grad, clip_mode=args.clip_mode,
+                parameters=model_parameters(model, exclude_head='agc' in args.clip_mode),
+                create_graph=second_order)
+        else:
+            loss.backward(create_graph=second_order)
+            if args.clip_grad is not None:
+                dispatch_clip_grad(
+                    model_parameters(model, exclude_head='agc' in args.clip_mode),
+                    value=args.clip_grad, mode=args.clip_mode)
+            optimizer.step()
+
+        torch.cuda.synchronize()
+        num_updates += 1
+        batch_time = time.time() - end
+        batch_time_m.update(batch_time)
+        step_duration_list.append(batch_time)
+        if last_batch or batch_idx % args.log_interval == 0:
+            lrl = [param_group['lr'] for param_group in optimizer.param_groups]
+            lr = sum(lrl) / len(lrl)
+
+            if args.distributed:
+                reduced_loss = reduce_tensor(loss.data, args.world_size)
+                losses_m.update(reduced_loss.item(), input.size(0))
+
+            if args.local_rank == 0:
+                print_if_verbose(
+                    'Train: {} [{:>4d}/{} ({:>3.0f}%)]  '
+                    'Loss: {loss.val:#.4g} ({loss.avg:#.3g})  '
+                    'Time: {batch_time.val:.3f}s, {rate:>7.2f}/s  '
+                    '({batch_time.avg:.3f}s, {rate_avg:>7.2f}/s)  '
+                    'LR: {lr:.3e}  '
+                    'Data: {data_time.val:.3f} ({data_time.avg:.3f})'.format(
+                        epoch,
+                        batch_idx, len(loader),
+                        100. * batch_idx / last_idx,
+                        loss=losses_m,
+                        batch_time=batch_time_m,
+                        rate=input.size(0) * args.world_size / batch_time_m.val,
+                        rate_avg=input.size(0) * args.world_size / batch_time_m.avg,
+                        lr=lr,
+                        data_time=data_time_m))
+
+        end = time.time()
+        # end for
+
+    return OrderedDict([('loss', losses_m.avg)])
+
+
+if __name__ == '__main__':
+    main()
