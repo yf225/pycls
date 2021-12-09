601619704c23084469faf86ed24b872fca10bf57
Will Feng (DPER) <willfeng@fb.com>
2021-12-08 23:04:28 -0800

    update

--- train_vit_pt_pycls_gpu.py
+++ train_vit_pt_pycls_gpu.py
@@ -166,7 +166,7 @@ class ViTEncoder(Module):
     def forward(self, x):
         for block in self.children():
             x = block(x)
-        return self.ln(x)
+        return x
 
 
 class ViTStemPatchify(Module):
